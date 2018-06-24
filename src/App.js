7ed064677dcb743e7b5f4f3eddf8f49d1408a0e1
Nik <niklaus0105@gmail.com>
2018-06-24 18:05:54 -0500

    header UI adjustments & import Register form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Header } from './Header';
 import './App.css';
-
+import { Register } from './Register';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+          <Register/>
       </div>
     );
   }
