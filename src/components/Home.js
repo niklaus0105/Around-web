641735fd4820c6807d58a040c777dff596c82e31
Nik <niklaus0105@gmail.com>
2018-07-01 16:59:41 -0500

    add get geo location

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import { Tabs, Button } from 'antd';
+import { GEO_OPTIONS } from '../constants';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button type="primary">Create New Post</Button>;
 
 export class Home extends React.Component {
+    componentDidMount() {
+        if ("geolocation" in navigator) {
+            /* geolocation is available */
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessLoadGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS);
+        } else {
+            /* geolocation IS NOT available */
+        }
+    }
+
+    onSuccessLoadGeoLocation = (position) => {
+        console.log(position)
+    }
+
+    onFailedLoadGeoLocation = () => {
+
+    }
     render() {
         return (
             <Tabs className="main-tabs" tabBarExtraContent={operations}>
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,9 @@
 //export const API_ROOT = 'http://localhost:8080/register';
 export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
 export const TOKEN_KEY = 'TOKEN_KEY';
+
+export const GEO_OPTIONS = {
+    enableHighAccuracy: true,
+    maximumAge        : 30000,
+    timeout           : 27000
+};
