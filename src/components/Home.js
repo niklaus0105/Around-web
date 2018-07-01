fb6a4a96e0377a032548cdcc7dd7ae8c375387d4
Nik <niklaus0105@gmail.com>
2018-07-01 17:42:24 -0500

    log geo-based events response from ajax request

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import $ from 'jquery';
 import { Tabs, Button } from 'antd';
-import { GEO_OPTIONS } from '../constants';
+import { GEO_OPTIONS, POS_KEY, API_ROOT, AUTH_PREFIX, TOKEN_KEY } from '../constants';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button type="primary">Create New Post</Button>;
@@ -19,12 +20,34 @@ export class Home extends React.Component {
     }
 
     onSuccessLoadGeoLocation = (position) => {
-        console.log(position)
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem(POS_KEY, JSON.stringify({ lat: latitude, lon: longitude}));
+        this.loadNearbyPosts();
     }
 
     onFailedLoadGeoLocation = () => {
 
     }
+
+    loadNearbyPosts = () => {
+        const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+
+        $.ajax({
+            url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=200000`,
+            method: 'GET',
+            headers: {
+                Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`
+            },
+        }).then((response) => {
+            console.log(response);
+        }, () => {
+
+        }).catch(() => {
+
+        });
+    }
+
     render() {
         return (
             <Tabs className="main-tabs" tabBarExtraContent={operations}>
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 //export const API_ROOT = 'http://localhost:8080/register';
-export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
+//export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
+export const API_ROOT = 'https://legacy-dot-around-75015.appspot.com/api/v1';
 export const TOKEN_KEY = 'TOKEN_KEY';
 
 export const GEO_OPTIONS = {
@@ -7,3 +8,6 @@ export const GEO_OPTIONS = {
     maximumAge        : 3600000,
     timeout           : 27000
 };
+
+export const POS_KEY = 'POS_KEY';
+export const AUTH_PREFIX = 'Bearer';
