a0ffb4dd9579c3f3f06c51773960e6e1a1cbfe6f
Nik <niklaus0105@gmail.com>
2018-07-01 16:32:26 -0500

    expand tab width to 80%

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,12 @@ import React from 'react'
 import { Tabs, Button } from 'antd';
 
 const TabPane = Tabs.TabPane;
-
 const operations = <Button type="primary">Create New Post</Button>;
 
 export class Home extends React.Component {
     render() {
         return (
-            <Tabs tabBarExtraContent={operations}>
+            <Tabs className="main-tabs" tabBarExtraContent={operations}>
                 <TabPane tab="Posts" key="1">Posts</TabPane>
                 <TabPane tab="Map" key="2">Map</TabPane>
             </Tabs>
